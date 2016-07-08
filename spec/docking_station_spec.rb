ae63c95590c6a901d3e37d0853e1a831bc62da0e
cyberplanner <saleh.gfx@gmail.com>
2016-07-08 01:36:08 +0100

    10_making_stations_release_bikes, Passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,18 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
+
+  context "Gets a bike, expect bike to be working?" do
+    it "gets a bike" do
+      expect(subject.release_bike).to be_an_instance_of(Bike)
+    end
+
+    it "bike working?" do
+      expect(subject.release_bike).to be_working
+    end
+
+  end
+
 end
