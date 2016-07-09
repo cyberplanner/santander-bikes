b626f421a4134f5bbe47d9dc1f08093fd86ea906
cyberplanner <saleh.gfx@gmail.com>
2016-07-09 18:03:10 +0100

    11.Using Instance Variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+  
+
+  def dock(bike)
+    @bike= bike
+  end
+
+  attr_reader :bike
+  
   def release_bike
     Bike.new
   end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,15 +3,39 @@ require 'docking_station'
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
 
-  context "Gets a bike, expect bike to be working?" do
-    it "gets a bike" do
-      expect(subject.release_bike).to be_an_instance_of(Bike)
-    end
 
-    it "is working?" do
-      expect(subject.release_bike).to be_working
-    end
-
+  it "gets a bike" do
+    expect(subject.release_bike).to be_an_instance_of(Bike)
   end
+
+  it "is working?" do
+    expect(subject.release_bike).to be_working
+  end
+
+
+  it { expect(subject).to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+    end
+
+  it "shows docks bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  
+
+  
+
+
+
+
+
+
 
 end
